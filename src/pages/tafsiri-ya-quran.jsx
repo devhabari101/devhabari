cb77df2c199b99b9c170d82b084288660d4e1cab
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-08-01 18:13:16 +0300

    Create tafsiri-ya-quran.jsx

---
+++ src/pages/tafsiri-ya-quran.jsx
@@ -0,0 +1,41 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+
+const TafsiriPage = ({ data }) => {
+  const { allMdx } = data
+
+  return (
+    <div>
+      <h1>Content List</h1>
+      <ul>
+        {allMdx.nodes.map(node => (
+          <li key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <MDXRenderer>{node.body}</MDXRenderer>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMdx(
+      filter: { frontmatter: { includeInList: { eq: true } } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      nodes {
+        id
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+        }
+        body
+      }
+    }
+  }
+`
+
+export default TafsiriPage
