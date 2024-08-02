021526eb2571f79fe49df01a420bf601ee890d56
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-08-02 17:46:03 +0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,61 +1,61 @@
-import React from 'react'
-//import Hero from '../components/Hero'
-import Layout from '../components/Layout'
-import RecentPosts from '../components/RecentPosts'
-import { graphql } from 'gatsby'
-import Featured from '../components/featured/Featured'
-import Seo from '../components/SEO'
-import Notification from '../components/Notification'
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import Seo from '../components/SEO';
+import RecentPosts from '../components/RecentPosts';
+import Featured from '../components/featured/Featured';
+import Notification from '../components/Notification';
 
 const IndexPage = ({ data }) => {
-  const {
-    allMdx: { nodes: posts },
-  } = data
-  const {
-    site: { siteMetadata: metadata },
-  } = data
+  const { allMdx: { nodes: posts } } = data;
+  const { site: { siteMetadata: metadata } } = data;
+
+  // Log to check filtered posts
+  console.log('Filtered posts:', posts);
+
   return (
     <Layout>
       <Seo
         title={`${metadata.title} | Tuna Hamasisha Elimu Ya Bure Kwa Wote`}
         description={metadata.description.substr(0, 160)}
       />
-
-      
-
       <Featured />
       <Notification />
       <RecentPosts posts={posts} />
     </Layout>
-  )
+  );
 }
 
 export const query = graphql`
-query Home {
-  allMdx(limit: 3, sort: {frontmatter: {date: DESC}}) {
-    nodes {
-      excerpt
-      frontmatter {
-        title
-        category
-        date(formatString: "MMMM, Do YYYY")
-        image {
-          childImageSharp {
-            gatsbyImageData
+  query Home {
+    allMdx(
+      limit: 3,
+      sort: { frontmatter: { date: DESC } },
+      filter: { frontmatter: { excludeFromIndex: { ne: true } } }
+    ) {
+      nodes {
+        excerpt
+        frontmatter {
+          title
+          category
+          date(formatString: "MMMM Do, YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
           }
         }
+        id
       }
-      id
+    }
+    site {
+      siteMetadata {
+        title
+        description
+        image
+      }
     }
   }
-  site {
-    siteMetadata {
-      title
-      description
-      image
-    }
-  }
-}
-`
+`;
 
-export default IndexPage
+export default IndexPage;
