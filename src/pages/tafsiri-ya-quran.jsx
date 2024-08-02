58d8e3f932963929153ac1e0891f2dc8538334c4
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-08-02 17:10:13 +0300

    Update and rename tafsiri-ya-quran.old to tafsiri-ya-quran.jsx

---
+++ src/pages/tafsiri-ya-quran.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/Layout';
+
+const TafsiriPage = () => (
+  <Layout>
+    <h1>Tafsiri ya quraan</h1>
+    <p>This is the about page.</p>
+    <h2>Related Articles</h2>
+    <ul>
+      <li><Link to="/list1">Go to List 1</Link></li>
+      
+    </ul>
+  </Layout>
+);
+
+export default TafsiriPage;
+
