224176d91f7a38ca6377818f4d8da7e09c965a6a
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-08-03 10:27:14 +0300

    Update tafsiri-ya-quran.jsx

--- src/pages/tafsiri-ya-quran.jsx
+++ src/pages/tafsiri-ya-quran.jsx
@@ -5,8 +5,10 @@ import Layout from '../components/Layout';
 const TafsiriPage = () => (
   <Layout>
     <h1>Tafsiri ya quraan</h1>
-    <p>This is the about page.</p>
-    <h2>Related Articles</h2>
+    <p>`وَأَنَّ هَـٰذَا صِرَاطِي مُسْتَقِيمًا فَاتَّبِعُوهُ ۖ  وَلَا تَتَّبِعُوا السُّبُلَ فَتَفَرَّقَ بِكُمْ عَن سَبِيلِهِ ۚ `
+
+`Na kwamba hii ndiyo njia Yangu iliyonyooka, basi ifuateni. Na wala msifuate njia nyinginezo zitakufarikisheni na Njia Yake.` [Al-An’aam (6:153)]</p>
+    
     <ul>
       <li><Link to="/example-category/list-1/">Go to List 1</Link></li>
       
