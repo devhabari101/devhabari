32207be2d9235a051145d62e6a5060ca4cdd6a2c
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-08-03 10:02:12 +0300

    Update RecentPosts.jsx

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -1,21 +1,14 @@
-import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
-import { AiOutlineClockCircle } from 'react-icons/ai'
-import { GoCalendar } from 'react-icons/go'
-import { Link } from 'gatsby'
-import SidebarWide from './Sidebar/indexWide'
-import { getColor } from './utils/heroCategories'
-import slugify from 'slugify'
-import '../components/featured/features-styles.scss'
+import React from 'react';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { AiOutlineClockCircle } from 'react-icons/ai';
+import { GoCalendar } from 'react-icons/go';
+import { Link } from 'gatsby';
+import SidebarWide from './Sidebar/indexWide';
+import { getColor } from './utils/heroCategories';
+import slugify from 'slugify';
+import '../components/featured/features-styles.scss';
 
-const RecentPosts = () => {
-  const data = useStaticQuery(query)
-
-  const {
-    allMdx: { nodes: posts },
-  } = data
-
+const RecentPosts = ({ posts }) => {
   return (
     <section className="container">
       <div className="row">
@@ -23,7 +16,7 @@ const RecentPosts = () => {
           <h2 className="section-title">Makala Mbalimbali</h2>
 
           {posts.map(post => {
-            const { title, category, date, slug, image } = post.frontmatter
+            const { title, category, date, slug, image } = post.frontmatter;
 
             return (
               <article
@@ -31,23 +24,18 @@ const RecentPosts = () => {
                 className="card card-bg card-shadow recent-article-mb"
               >
                 <Link
-                  to={`/${category.toLowerCase()}/${
-                    slug || slugify(title, { lower: true })
-                  }`}
+                  to={`/${category.toLowerCase()}/${slug || slugify(title, { lower: true })}`}
                 >
                   <GatsbyImage
                     image={getImage(image)}
                     alt={title}
                     className="img"
                   />
-                  {/* <img className='top-and-popular-img' src={imageSource} alt={title} /> */}
                 </Link>
 
                 <div className="card-body">
                   <Link
-                    to={`/${category.toLowerCase()}/${
-                      slug || slugify(title, { lower: true })
-                    }`}
+                    to={`/${category.toLowerCase()}/${slug || slugify(title, { lower: true })}`}
                   >
                     <h3 className="recent-articles-heading">{title}</h3>
                   </Link>
@@ -80,55 +68,25 @@ const RecentPosts = () => {
                     </li>
                   </ul>
                   <Link
-                    to={`/${category.toLowerCase()}/${
-                      slug || slugify(title, { lower: true })
-                    }`}
+                    to={`/${category.toLowerCase()}/${slug || slugify(title, { lower: true })}`}
                   >
                     <p className="excerpt">{post.excerpt}</p>
                   </Link>
                   <a
                     className="btn btn-outline-primary"
-                    href={`/${category.toLowerCase()}/${slugify(title, {
-                      lower: true,
-                    })}`}
+                    href={`/${category.toLowerCase()}/${slugify(title, { lower: true })}`}
                   >
                     Soma Zaidi
                   </a>
                 </div>
               </article>
-            )
+            );
           })}
         </div>
         <SidebarWide />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export const query = graphql`
-  query RecentPosts {
-    allMdx(sort: { frontmatter: { date: DESC } }, limit: 300) {
-      nodes {
-        excerpt
-        frontmatter {
-          title
-          category
-          date(formatString: "MMMM, Do YYYY")
-          image {
-            childImageSharp {
-              gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
-            }
-          }
-        }
-        id
-        fields {
-          timeToRead {
-            words
-          }
-        }
-      }
-    }
-  }
-`
-
-export default RecentPosts
+export default RecentPosts;
