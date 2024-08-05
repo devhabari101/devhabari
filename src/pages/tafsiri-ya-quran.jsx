4d437a88c347546455f2d442bc1771fa1036226e
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-08-05 11:03:25 +0300

    Update tafsiri-ya-quran.jsx

--- src/pages/tafsiri-ya-quran.jsx
+++ src/pages/tafsiri-ya-quran.jsx
@@ -12,6 +12,7 @@ const TafsiriPage = () => (
       <li><Link to="/quran/al-fatiha/">Al-FATIHA</Link></li>
       <li><Link to="/quran/al-adiyat/">AL-ADIYAT</Link></li>
       <li><Link to="/quran/al-qaria/">AL-QARIA</Link></li>
+      <li><Link to="/quran/at-takathur/">AT-TAKATHUR</Link></li>
     </ul>
   </Layout>
 );
