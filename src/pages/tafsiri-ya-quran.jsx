cd9d60020a06a79528537ae120e248170a527a53
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-08-05 11:22:05 +0300

    Update tafsiri-ya-quran.jsx

--- src/pages/tafsiri-ya-quran.jsx
+++ src/pages/tafsiri-ya-quran.jsx
@@ -3,17 +3,37 @@ import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 
 const TafsiriPage = () => (
-  <Layout>
-    <h1>Tafsiri ya quraan</h1>
-  <p> وَأَنَّ هَـٰذَا صِرَاطِي مُسْتَقِيمًا فَاتَّبِعُوهُ ۖ  وَلَا تَتَّبِعُوا السُّبُلَ فَتَفَرَّقَ بِكُمْ عَن سَبِيلِهِ ۚ</p>
 
-<p>Na kwamba hii ndiyo njia Yangu iliyonyooka, basi ifuateni. Na wala msifuate njia nyinginezo zitakufarikisheni na Njia Yake. [Al-An’aam (6:153)]</p>
-    <ul>
-      <li><Link to="/quran/al-fatiha/">Al-FATIHA</Link></li>
-      <li><Link to="/quran/al-adiyat/">AL-ADIYAT</Link></li>
-      <li><Link to="/quran/al-qaria/">AL-QARIA</Link></li>
-      <li><Link to="/quran/at-takathur/">AT-TAKATHUR</Link></li>
-    </ul>
+ <div className="container support-container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="section-title categories-title">Tafsiri ya quraan</h1>
+          
+              
+              <p> وَأَنَّ هَـٰذَا صِرَاطِي مُسْتَقِيمًا فَاتَّبِعُوهُ ۖ  وَلَا تَتَّبِعُوا السُّبُلَ فَتَفَرَّقَ بِكُمْ عَن سَبِيلِهِ ۚ</p>
+              <p>Kufikia azima hiyo, haya ni maono na mikakati ya DevHabari:</p>
+
+             <p>Na kwamba hii ndiyo njia Yangu iliyonyooka, basi ifuateni. Na wala msifuate njia nyinginezo zitakufarikisheni na Njia Yake. [Al-An’aam (6:153)]</p>
+             <ul>
+             <li><Link to="/quran/al-fatiha/">Al-FATIHA</Link></li>
+             <li><Link to="/quran/al-adiyat/">AL-ADIYAT</Link></li>
+             <li><Link to="/quran/al-qaria/">AL-QARIA</Link></li>
+             <li><Link to="/quran/at-takathur/">AT-TAKATHUR</Link></li>
+            </ul>
+            </div>
+          </div>
+         </div>
+
+
+
+
+
+
+  
+  <Layout>
+    <h1></h1>
+
+
   </Layout>
 );
 
