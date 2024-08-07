d03661cb449d053fcc979d931b4ee35bb37be581
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-08-07 18:05:36 +0300

    Update SoundCloudEmbed.js

--- src/components/SoundCloudEmbed.js
+++ src/components/SoundCloudEmbed.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const SoundCloudEmbedWrapper = styled.div`
   width: 50%;
-  height: 200px; /* Adjust this value to get the desired height */
+  height: 400px; /* Adjust this value to get the desired height */
   position: relative;
   overflow: hidden;
   
